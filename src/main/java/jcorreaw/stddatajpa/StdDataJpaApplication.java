10cc9dc4aa3c26891e525ccdaef4d754d0ee5d23
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-17 11:10:03 -0300

    primeiro projeto

---
+++ src/main/java/jcorreaw/stddatajpa/StdDataJpaApplication.java
@@ -0,0 +1,13 @@
+package jcorreaw.stddatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdDataJpaApplication.class, args);
+	}
+
+}
